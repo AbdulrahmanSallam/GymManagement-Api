c90d7db6f94f150a18ef4c0d3494fc883136012f
Abdulrahman Sallam <abdulrahman.sallam.official@gmail.com>
2026-04-22 19:19:16 +0200

    Add exception handler.

--- src/GymManagement.Api/Program.cs
+++ src/GymManagement.Api/Program.cs
@@ -3,25 +3,28 @@ using GymManagement.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddControllers();
-
-builder.Services.AddApplication().AddInfrastructure(builder.Configuration);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+
+    builder.Services.AddOpenApi();
+    builder.Services.AddControllers();
+    builder.Services.AddProblemDetails();
+
+    builder.Services.AddApplication().AddInfrastructure(builder.Configuration);
 }
 
-app.UseHttpsRedirection();
+var app = builder.Build();
+{
 
-app.MapControllers();
+    app.UseExceptionHandler();
 
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
 
-app.Run();
+    app.UseHttpsRedirection();
+    app.MapControllers();
+
+    app.Run();
+}
 
